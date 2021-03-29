f3b10043da6548ad2aecc5e2adc4714f945415aa
ido evergreen <dicksonevergreenido@gmail.com>
2021-03-29 15:10:01 +0100

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import firebase from "firebase"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDHbrQ2eaRY3BlcwZBL7he9Ntqh1QwRE1g",
+  authDomain: "todo-app-2bd4a.firebaseapp.com",
+  databaseURL: "https://todo-app-2bd4a.firebaseio.com",
+  projectId: "todo-app-2bd4a",
+  storageBucket: "todo-app-2bd4a.appspot.com",
+  messagingSenderId: "884213506436",
+  appId: "1:884213506436:web:d5258fcb81c490ebf3bdd1",
+  measurementId: "G-FWLKFTDLGJ"
+
+
+  
 })
 
 export const auth = app.auth()

ab85e9d773646d5b5166e841d6e76e55e8e7a0c0
ido evergreen <dicksonevergreenido@gmail.com>
2021-04-02 14:50:47 +0100

    implemeting truncate for folder name

--- src/components/drive/folder.js
+++ src/components/drive/folder.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import CreateNewFolderIcon from "@material-ui/icons/CreateNewFolder";
+// import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Typography from '@material-ui/core/Typography';
 
-export default function folder({folder}) {
-    return (
-        <div>
-            {folder.name}
-        </div>
-    )
+import Link from '@material-ui/core/Link';
+
+export default function folder({ folder }) {
+  return (
+
+    <Typography noWrap >
+   <Link
+      component="button"
+      variant="body2"
+      noWrap
+      >
+        <CreateNewFolderIcon />
+        {folder.name}
+      </Link>
+    </Typography>
+   
+
+  );
 }
